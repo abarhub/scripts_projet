5c194d2757c920a104ab282350fe0ad8685e3d9a
abarhub <abarhub@gmail.com>
2024-12-14 23:10:10 +0100

    ajout de la fonction pour l'analyse de git

--- liste_projet.ps1
+++ liste_projet.ps1
@@ -101,17 +101,70 @@ function Read-Package-Dependencies {
     return $CombinedTable
 }
 
-function Read-Package-DevDependencies {
-    $file=$args[0]
-    $json = Get-Content $file | ConvertFrom-Json 
+function Get-GitBranchInfo {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$RepositoryPath
+    )
 
-    $json.devDependencies.psobject.properties | ForEach-Object {
-        [PSCustomObject]@{
-            'Name' = $_.name
-            'Version'    = $_.value
-            'Path'=$file
-        }
+    # Vérification si le chemin spécifié est un dépôt Git
+    if (-not (Test-Path "$RepositoryPath\.git")) {
+        Write-Error "$RepositoryPath n'est pas un dépôt Git valide."
+        return
     }
 
+    # Enregistrement du répertoire actuel pour y revenir ensuite
+    $currentPath = Get-Location
+    Set-Location -Path $RepositoryPath
+
+    try {
+        # Effectuer un fetch pour s'assurer que les données sont à jour
+        git fetch 2>$null
+
+        # Récupération des informations nécessaires
+        $branchName = git rev-parse --abbrev-ref HEAD 2>$null
+        $branchDate = git log -1 --format="%ci" 2>$null
+        $commitHash = git rev-parse HEAD 2>$null
+        $shortCommitHash = git rev-parse --short HEAD 2>$null
+        $commitMessage = git log -1 --format="%s" 2>$null
+        $status = git status --porcelain=2 --branch 2>$null
+
+        # Analyse des informations sur la synchronisation avec la branche distante
+        $isUpToDate = $false
+        $hasModifications = $false
+        $behind = $status -match "behind ([0-9]+)"
+        $ahead = $status -match "ahead ([0-9]+)"
+
+        if (-not $behind -and -not $ahead) {
+            $isUpToDate = $true
+        }
+
+        # Vérifier les modifications locales
+        if ($status -match "1 .*") {
+            $hasModifications = $true
+        }
+
+        # Construire l'objet de sortie
+        $branchInfo = [PSCustomObject]@{
+            BranchName        = $branchName
+            BranchDate        = $branchDate
+            CommitHash        = $commitHash
+            ShortCommitHash   = $shortCommitHash
+            CommitMessage     = $commitMessage
+            HasModifications  = $hasModifications
+            IsUpToDate        = $isUpToDate
+        }
+
+        return $branchInfo
+
+    } catch {
+        Write-Error "Une erreur s'est produite : $_"
+    } finally {
+        # Retourner au répertoire initial
+        Set-Location -Path $currentPath
+    }
 }
+
+
+
 
